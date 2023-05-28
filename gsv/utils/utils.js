c0339a5649988a585beb04aa6f91e1c65e0942e4
Morgan-Wilkinson <46545893+Morgan-Wilkinson@users.noreply.github.com>
2023-05-27 20:51:30 -0400

    Updates

--- gsv/utils/utils.js
+++ gsv/utils/utils.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import { auth } from "../firebase/app";
 import { useAuthState } from "react-firebase-hooks/auth";
-import getData from "/firebase/firestore/getData";
+import User from "../models/User";
 
 export function GetCurrentURL() {
   return window.location.href;
@@ -17,19 +17,24 @@ export const AdminPage = () => {
   const [user, loading, error] = useAuthState(auth);
 
   let adminPagePathRoot = "vendor-dashboard";
-  if (user && user.uid) {
-    const result = getData("users", user.uid);
-
-    if (
-      result != null &&
-      result.role != "admin" &&
-      router.pathname.includes(adminPagePathRoot)
-    ) {
+  if (user != null && user.uid != null && typeof window !== "undefined") {
+    if (User == null) {
+      new User();
+    }
+    console.log(User.admin);
+    if (User.admin != true && router.pathname.includes(adminPagePathRoot)) {
       return router.push("/");
     } else {
       return true;
     }
   } else {
     return false;
+  }
+};
+
+export const GetUserData = () => {
+  const [user, loading, error] = useAuthState(auth);
+  if (user && user.uid && typeof window !== "undefined") {
+    const result = getData("users", user.uid);
   }
 };
