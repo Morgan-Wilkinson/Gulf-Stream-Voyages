320e195ea0d2a72cad4f51dfdd8d89021975102e
Morgan-Wilkinson <46545893+Morgan-Wilkinson@users.noreply.github.com>
2023-06-07 20:39:25 -0400

    Update to form

---
+++ gsv/pages/vendor-dashboard/create-an-internal-user/components/InternalUserForm.jsx
@@ -0,0 +1,192 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, RegisterWithEmailAndPassword } from "../../../../firebase/app";
+
+function InternalUserForm() {
+  const router = useRouter();
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [confirmationPassword, setConfirmationPassword] = React.useState("");
+  const [shownError, setError] = React.useState("");
+  const [user, loading, error] = useAuthState(auth);
+  const errorCodeMessage = new Map([
+    [
+      "auth/account-exists-with-different-credential",
+      "There is already an Account associated with this crendetial",
+    ],
+    ["auth/email-already-in-use", "Email already in use"],
+    ["auth/invalid-email", "Invalid email"],
+    ["auth/operation-not-allowed", "Sign up method currently not supported"],
+    ["auth/weak-password", "Weak password"],
+    [
+      "auth/user-disabled",
+      "User currently disabled please contact technical support at admin@gulfstreamvoyages.com",
+    ],
+    ["auth/user-not-found", "User not found"],
+    ["auth/wrong-password", "Incorrect password"],
+    ["auth/invalid-verification-code", "Invalid Verification Code"],
+    ["auth/invalid-verification-id", "Invalid Verification Id"],
+    ["auth/invalid-credential", "Expired credentials"],
+  ]);
+
+  const handleForm = async (event) => {
+    event.preventDefault();
+
+    // Reset the error before trying to submit the form
+    if (error) setError("");
+    if (shownError) setError("");
+
+    // Check passwords match
+    if (password !== confirmationPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    // Check password format
+    const passwordRegex =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,256}$/gm;
+
+    if (!passwordRegex.test(password)) {
+      setError(
+        "Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character."
+      );
+      return;
+    }
+
+    RegisterWithEmailAndPassword(
+      firstName + " " + lastName,
+      email,
+      password
+    ).then((result) => {
+      if (auth.currentUser) {
+        router.push("/");
+      } else {
+        setError(result.error);
+      }
+    });
+  };
+
+  return (
+    <form onSubmit={handleForm} className="form row y-gap-20">
+      <div className="d-flex align-items-center">
+        <div class="col-3 d-inline-block">
+          <h1 className="text-22 fw-500">User Account Type</h1>
+        </div>
+        <div class="col-1 d-inline-block">
+          <button
+            class="btn btn-secondary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Select a User Account Type
+          </button>
+          <ul class="dropdown-menu">
+            <li>
+              <a class="dropdown-item" href="#">
+                Admin
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                Internal
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* End .col */}
+
+      <div className="col-12">
+        <div className="form-input ">
+          <input
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+            type="text"
+            name="firstName"
+            id="firstName"
+          />
+          <label className="lh-1 text-14 text-light-1">First Name</label>
+        </div>
+      </div>
+      {/* End .col */}
+
+      <div className="col-12">
+        <div className="form-input ">
+          <input
+            onChange={(e) => setLastName(e.target.value)}
+            required
+            type="text"
+            name="lastName"
+            id="lastName"
+          />
+          <label className="lh-1 text-14 text-light-1">Last Name</label>
+        </div>
+      </div>
+      {/* End .col */}
+
+      <div className="col-12">
+        <div className="form-input ">
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            type="email"
+            name="email"
+            id="email"
+          />
+          <label className="lh-1 text-14 text-light-1">Email</label>
+        </div>
+      </div>
+      {/* End .col */}
+
+      <div className="col-12">
+        <div className="form-input ">
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            type="password"
+            name="password"
+            id="password"
+          />
+          <label className="lh-1 text-14 text-light-1">Password</label>
+        </div>
+      </div>
+      {/* End .col */}
+
+      <div className="col-12">
+        <div className="form-input ">
+          <input
+            onChange={(e) => setConfirmationPassword(e.target.value)}
+            required
+            type="password"
+            name="confirmationPassword"
+            id="confirmationPassword"
+          />
+          <label className="lh-1 text-14 text-light-1">Confirm Password</label>
+        </div>
+      </div>
+      {/* End .col */}
+
+      <div>
+        <p style={{ color: "red" }}>{shownError}</p>
+      </div>
+      {/*Error Message*/}
+      <div className="col-12">
+        <button
+          type="submit"
+          className="button py-20 -dark-1 bg-blue-1 text-white w-100"
+        >
+          Sign Up <div className="icon-arrow-top-right ml-15" />
+        </button>
+      </div>
+      {/* End .col */}
+    </form>
+  );
+}
+
+export default InternalUserForm;
